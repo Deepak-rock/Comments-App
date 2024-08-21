079c0327f2399b5b87a1f2b53cb10fdf2823e5f4
Deepak-rock <deepakind777@gmail.com>
2024-08-21 20:23:54 +0530

    test-2

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,62 @@
 // Write your code here
+
+import {formatDistanceToNow} from 'date-fns'
+
+const CommentItem = props => {
+  const {commentsDetails, toggleIsLike, commentDelete} = props
+  const {name, id, comment, isLiked, initialContainer, date} = commentsDetails
+  const initial = name ? name[0].topUpperCase : ''
+  const postedTime = formatDistanceToNow(date)
+  const liketextClassName = isLiked ? 'active button' : 'button'
+  const imageUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+
+  const onClickDelete = () => {
+    commentDelete(id)
+  }
+  const onClickToggle = () => {
+    toggleIsLike(id)
+  }
+
+  return (
+    <li className="comment-item">
+      <div className="comment-container">
+        <div className={initialContainer}>
+          <p className="initial">{initial}</p>
+        </div>
+        <div className="name-time-container">
+          <p className="name">{name}</p>
+          <p className="time">{postedTime}</p>
+        </div>
+        <p className="comment-text">{comment}</p>
+      </div>
+      <div className="button-container">
+        <div className="like-button-container">
+          <img src={imageUrl} alt="like" className="like-icon" />
+          <button
+            className={liketextClassName}
+            onClick={onClickToggle}
+            type="button"
+          >
+            Like
+          </button>
+        </div>
+        <button
+          className="delete-button"
+          onClick={onClickDelete}
+          type="button"
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
+      <hr className="line" />
+    </li>
+  )
+}
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,3 +1,8 @@
+import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import CommentItem from '../CommentItem/index'
+import './index.css'
+
 const initialContainerBackgroundClassNames = [
   'amber',
   'blue',
@@ -9,3 +14,121 @@ const initialContainerBackgroundClassNames = [
 ]
 
 // Write your code here
+class Comments extends Component {
+  state = {
+    nameInput: '',
+    commentInput: '',
+    commentsList: [],
+  }
+
+  toggleIsLike = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachComment => {
+        if (eachComment === id) {
+          return {...eachComment, isLiked: !eachComment.isLiked}
+        }
+        return eachComment
+      }),
+    }))
+  }
+
+  commentDelete = id => {
+    const {commentsList} = this.state
+
+    this.setState({
+      commentsList: commentsList.filter(comment => comment.id !== id),
+    })
+  }
+
+  onChangeName = event => {
+    this.setState({nameInput: event.target.value})
+  }
+
+  onChangeComment = event => {
+    this.setState({commentInput: event.target.value})
+  }
+
+  AddCommentsList = () => {
+    const {nameInput, commentInput} = this.state
+    const initialContainerBackgroundClassName = `initail-container ${
+      initialContainerBackgroundClassNames[
+        Math.ceil(
+          Math.random() * initialContainerBackgroundClassNames.length - 1,
+        )
+      ]
+    }`
+    const newCommentList = {
+      id: uuidv4(),
+      name: nameInput,
+      comment: commentInput,
+      date: new Date(),
+      isLiked: false,
+      initialContainer: initialContainerBackgroundClassName,
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newCommentList],
+      nameInput: '',
+      commentInput: '',
+    }))
+  }
+
+  renderCommentsList = () => {
+    const {commentsList} = this.state
+    return commentsList.map(eachComment => (
+      <CommentItem
+        key={eachComment.id}
+        commentsDetails={eachComment}
+        toggleIsLike={this.toggleIsLike}
+        commentDelete={this.commentDelete}
+      />
+    ))
+  }
+
+  render() {
+    const {nameInput, commentInput, commentsList} = this.state
+    const count = commentsList.length
+    return (
+      <div className="app-contianer">
+        <div className="comment-content-container">
+          <h1 className="heading">Comments</h1>
+          <div className="commnts-inputs">
+            <form className="from" onSubmit={this.AddCommentsList}>
+              <p className="para-text">Say something about 4.0 Technologies</p>
+              <input
+                type="text"
+                value={nameInput}
+                placeholder="Your Name"
+                className="name-input"
+                onChange={this.onChangeName}
+              />
+              <br />
+              <textarea
+                type="text"
+                rows="6"
+                value={commentInput}
+                placeholder="Your Name"
+                className="comment-input"
+                onChange={this.onChangeComment}
+              />
+              <br />
+              <button className="add-button" type="submit">
+                Add Comment
+              </button>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              alt="comments"
+              className="image"
+            />
+          </div>
+          <hr className="line" />
+          <p className="comments-count">
+            <span className="count-container">{count}</span>Comments
+          </p>
+          <ul className="comment-list">{this.renderCommentsList()}</ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Comments
